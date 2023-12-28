09b6ba106837177c5af6275a0572113d61f770b9
AlexisKcc8 <CodeKJS863@gmail.com>
2023-12-27 22:53:51 -0600

    seccion hero agregada y estilizada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // standalone: true, //?standalone nos ayuda con las importaciones de componentes
+  // imports: [], //? aqui es donde pondremos el nombre del componente o modulo que necesitemos usar dentro de este componente
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'la-esperanza';
+  title = 'esperanza:Comida Ec';
 }
